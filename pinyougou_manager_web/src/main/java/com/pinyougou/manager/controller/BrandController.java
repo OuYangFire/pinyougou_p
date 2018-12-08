252624cf030a938992f62781564b4a95dd09cf57
欧阳伟伟 <727084648@qq.com>
2018-12-08 13:30:55 +0800

    findPage.do 未加插件

--- pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -3,6 +3,7 @@ package com.pinyougou.manager.controller;
 import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,16 @@ import com.pinyougou.sellergoods.service.BrandService;
 @RequestMapping("/brand")
 public class BrandController {
 
-	@Reference
-	private BrandService brandService;
-	
-	@RequestMapping("/findAll")
-	public List<TbBrand> findAll(){
-		return brandService.findAll();		
-	}
-	
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/findAll")
+    public List<TbBrand> findAll() {
+        return brandService.findAll();
+    }
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page,int size){
+        return brandService.findPage(page, size);
+    }
 }
